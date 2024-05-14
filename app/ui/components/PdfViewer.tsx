f7e9c40438ec712b9731d3a16d10bf426094ace8
Anderson Torres <106359275+And3rsonTorres@users.noreply.github.com>
2024-05-14 13:01:04 -0400

    PDFVieweer
    -fixed PDF issue

--- app/ui/components/PdfViewer.tsx
+++ app/ui/components/PdfViewer.tsx
@@ -8,7 +8,6 @@ import { useEffect, useState } from "react";
 import { pdfjs, Document, Page } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 import "react-pdf/dist/esm/Page/TextLayer.css";
-import type { PDFDocumentProxy } from "pdfjs-dist";
 
 pdfjs.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 interface File {
@@ -50,7 +49,9 @@ export default function PDFFileViewer(file: File) {
 
   function onDocumentLoadSuccess({
     numPages: nextNumPages,
-  }: PDFDocumentProxy): void {
+  }: {
+    numPages: number;
+  }): void {
     setNumPages(nextNumPages);
   }
 
